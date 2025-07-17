5843e15ad92ad732df78a844ddfb5d064ce2a754
Albert M. Canoy <80763419+Canz-143@users.noreply.github.com>
2025-07-17 09:44:21 +0800

    Update gemini.py

--- app/services/gemini.py
+++ app/services/gemini.py
@@ -6,7 +6,7 @@ from google.genai.types import GenerateContentConfig
 from app.config import GOOGLE_API_KEY
 
 client = genai.Client()
-model_id = "gemini-2.0-flash"
+model_id = "gemini-2.5-flash"
 search_tool = Tool(google_search=GoogleSearch())
 
 config = GenerateContentConfig(
