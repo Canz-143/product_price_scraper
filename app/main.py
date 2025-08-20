16fbeffca8929c582e1aa7f21ce192f611541308
bert143-oss <albert.canoy@hmr.ph>
2025-08-20 12:30:58 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.routes import product
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # Include product routes
 app.include_router(product.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or specify allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
